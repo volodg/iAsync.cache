9e0b80367193217c054ac05fa5e1ec97a03607e1
volodg <gorbenko.vova@gmail.com>
2015-06-24 23:01:31 +0300

    initial lib commit

---
+++ iAsync.cache/iAsync_cache.h
@@ -0,0 +1,19 @@
+//
+//  iAsync.cache.h
+//  iAsync.cache
+//
+//  Created by Volodymyr on 24.06.15.
+//  Copyright © 2015 EmbeddedSources. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iAsync.cache.
+FOUNDATION_EXPORT double iAsync_cacheVersionNumber;
+
+//! Project version string for iAsync.cache.
+FOUNDATION_EXPORT const unsigned char iAsync_cacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iAsync_cache/PublicHeader.h>
+
+#import "iAsync_cache-Bridging-Header.h"
