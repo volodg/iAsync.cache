af34bfad7d40c3df936dbc43060d9cce8c0cde61
volodg <volodg@mail.ru>
2015-06-30 12:55:16 +0300

    use runAsync func

--- iAsync.cache/Categories/UIImageView+CachedAsyncImageLoader.swift
+++ iAsync.cache/Categories/UIImageView+CachedAsyncImageLoader.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+import iAsync_async
 import iAsync_utils
 
 import UIKit
@@ -59,10 +60,7 @@ public extension UIImageView {
             
             let storage = jThumbnailStorage
             let loader  = storage.thumbnailLoaderForUrl(url)
-            let cancel  = loader(
-                progressCallback: nil,
-                stateCallback   : nil,
-                finishCallback  : doneCallback)
+            runAsync(loader, doneCallback)
         }
     }
 }
--- iAsync.cache/Details/JInternalCacheDB.swift
+++ iAsync.cache/Details/JInternalCacheDB.swift
@@ -70,10 +70,7 @@ internal class JInternalCacheDB : JKeyValueDB, JCacheDB {
                 let queueName = "com.embedded_sources.dbcache.thread_to_remove_old_data"
                 let loader = asyncWithSyncOperationAndQueue(loadDataBlock, queueName)
                 
-                let cancel = loader(
-                    progressCallback: nil,
-                    stateCallback: nil,
-                    finishCallback: { (result: Result<NSNull, NSError>) in
+                runAsync(loader, { (result: Result<NSNull, NSError>) in
                     
                     result.error?.writeErrorWithJLogger()
                 })
