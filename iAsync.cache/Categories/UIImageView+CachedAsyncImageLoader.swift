cbcff5ec1fe223e70446adc1cba1f374378ce3c6
vlg <gorbenko.vova@gmail.com>
2016-01-26 17:01:14 +0200

    add callbak on finish

--- iAsync.cache/Categories/UIImageView+CachedAsyncImageLoader.swift
+++ iAsync.cache/Categories/UIImageView+CachedAsyncImageLoader.swift
@@ -16,7 +16,7 @@ import UIKit
 private var iAsync_AsycImageURLHolder: Void?
 
 public extension UIImageView {
-    
+
     private var iAsync_cache_AsycImageURL: NSURL? {
         get {
             return objc_getAssociatedObject(self, &iAsync_AsycImageURLHolder) as? NSURL
@@ -25,17 +25,17 @@ public extension UIImageView {
             objc_setAssociatedObject(self, &iAsync_AsycImageURLHolder, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
-    
+
     private func jffSetImage(image: UIImage?, url: NSURL) {
-        
+
         if image == nil || iAsync_cache_AsycImageURL != url {
             return
         }
-        
+
         self.image = image
     }
-    
-    func setImageWithURL(url: NSURL?, placeholder: UIImage? = nil, noImage: UIImage? = nil) {
+
+    func setImageWithURL(url: NSURL?, placeholder: UIImage? = nil, noImage: UIImage? = nil, callBack:((UIImage?)->Void)? = nil) {
 
         image = placeholder
 
@@ -49,13 +49,22 @@ public extension UIImageView {
 
             switch result {
             case .Success(let value):
+
                 let image = value
+
+                callBack?(image)
+
                 self_.jffSetImage(image, url:url)
             case .Failure:
+
+                callBack?(noImage)
+
                 self_.jffSetImage(noImage, url:url)
             case .Interrupted:
+                callBack?(nil)
                 break
             case .Unsubscribed:
+                callBack?(nil)
                 break
             }
         }
