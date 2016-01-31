f32de340080a8a3e161c7290fa32a14327cf5330
vlg <gorbenko.vova@gmail.com>
2016-01-31 23:40:46 +0200

    code style refactor

--- iAsync.cache/Errors/CacheLoadImageError.swift
+++ iAsync.cache/Errors/CacheLoadImageError.swift
@@ -11,11 +11,11 @@ import Foundation
 import iAsync_utils
 
 final public class CacheLoadImageError : CacheError {
-    
+
     let nativeError: NSError
-    
+
     required public init(nativeError: NSError) {
-        
+
         self.nativeError = nativeError
         super.init(description: "J_CACHE_LOAD_IMAGE_ERROR")
     }
@@ -23,19 +23,20 @@ final public class CacheLoadImageError : CacheError {
     required public init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     public override func copyWithZone(zone: NSZone) -> AnyObject {
-        
+
         return self.dynamicType.init(nativeError: nativeError)
     }
-    
+
     override public var errorLogDescription: String {
+
         let result = "\(self.dynamicType) : \(localizedDescription), domain : \(domain) code : \(code) nativeError: \(nativeError.errorLogDescription)"
         return result
     }
-    
+
     public override func writeErrorWithLogger () {
-        
+
         iAsync_utils_logger.logError(errorLogDescription)
     }
 }
