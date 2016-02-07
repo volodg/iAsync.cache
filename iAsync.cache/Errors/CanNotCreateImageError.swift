e92d89e4e9c852e0117115d0090b61883668c1ab
vlg <gorbenko.vova@gmail.com>
2016-02-07 16:34:53 +0300

    refactor with reactiveKit

--- iAsync.cache/Errors/CanNotCreateImageError.swift
+++ iAsync.cache/Errors/CanNotCreateImageError.swift
@@ -8,29 +8,27 @@
 
 import Foundation
 
-import iAsync_utils
-
 final public class CanNotCreateImageError : CacheError {
-    
+
     private let url: NSURL
-    
+
     public required init(url: NSURL) {
-        
+
         self.url = url
         super.init(description: "can not create image with given data")
     }
-    
+
     required public init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     public override func copyWithZone(zone: NSZone) -> AnyObject {
-        
+
         return self.dynamicType.init(url: url)
     }
-    
+
     override public func writeErrorWithLogger() {}
-    
+
     override public var errorLogDescription: String {
         return "\(self.dynamicType) : \(localizedDescription) url: \(url)"
     }
