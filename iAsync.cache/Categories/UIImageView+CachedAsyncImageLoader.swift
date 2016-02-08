59bdcc66893bea55a692ce41af6208fcb3a41fc0
vlg <gorbenko.vova@gmail.com>
2016-02-08 19:16:42 +0300

    refactor with using of reactiveKit

--- iAsync.cache/Categories/UIImageView+CachedAsyncImageLoader.swift
+++ iAsync.cache/Categories/UIImageView+CachedAsyncImageLoader.swift
@@ -8,46 +8,43 @@
 
 import Foundation
 
-import iAsync_async
 import iAsync_utils
 
 import UIKit
+import ReactiveKit
 
-private var iAsync_AsycImageURLHolder: Void?
+private var iAsync_AsycImageProperties: Void?
+
+private class Properties {
+    var dispose = DisposeBag()
+}
 
 public extension UIImageView {
 
-    private var iAsync_cache_AsycImageURL: NSURL? {
+    private var iAsync_cache_Properties: Properties {
         get {
-            return objc_getAssociatedObject(self, &iAsync_AsycImageURLHolder) as? NSURL
+            if let result = objc_getAssociatedObject(self, &iAsync_AsycImageProperties) as? Properties {
+                return result
+            }
+            let result = Properties()
+            self.iAsync_cache_Properties = result
+            return result
         }
         set (newValue) {
-            objc_setAssociatedObject(self, &iAsync_AsycImageURLHolder, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            objc_setAssociatedObject(self, &iAsync_AsycImageProperties, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
-    }
-
-    private func jffSetImage(image: UIImage?, url: NSURL) {
-
-        if image == nil || iAsync_cache_AsycImageURL != url {
-            return
-        }
-
-        self.image = image
     }
 
     func setImageWithURL(url: NSURL?, placeholder: UIImage? = nil, noImage: UIImage? = nil, callBack:((UIImage?)->Void)? = nil) {
 
         image = placeholder
 
-        iAsync_cache_AsycImageURL = url
-
         guard let url = url else { return }
 
         let onSuccess = { [weak self] (result: UIImage) -> () in
 
-            let image = result
-            callBack?(image)
-            self?.jffSetImage(image, url:url)
+            callBack?(result)
+            self?.image = result
         }
         let onFailure = { [weak self] (error: NSError) -> () in
 
@@ -57,11 +54,13 @@ public extension UIImageView {
             }
 
             callBack?(noImage)
-            self?.jffSetImage(noImage, url:url)
+            self?.image = noImage
         }
 
-        let thumb = thumbnailStorage.thumbnailStreamForUrl(url)
+        iAsync_cache_Properties.dispose.dispose()
+
+        let thumb  = thumbnailStorage.thumbnailStreamForUrl(url)
         let stream = thumb.on(success: { onSuccess($0) }, failure: { onFailure($0) })
-        stream.run()
+        stream.run().disposeIn(iAsync_cache_Properties.dispose)
     }
 }
