f041720d2b9ab55aececb5ed851a4f64a55c2fd0
vlg <gorbenko.vova@gmail.com>
2016-05-27 23:15:26 +0300

    use unicode char "©" instead of (c)

--- iAsync.cache/Categories/AsyncStreamType+CacheAdditions.swift
+++ iAsync.cache/Categories/AsyncStreamType+CacheAdditions.swift
@@ -3,7 +3,7 @@
 //  iAsync_cache
 //
 //  Created by Gorbenko Vladimir on 18/02/16.
-//  Copyright (c) 2016 EmbeddedSources. All rights reserved.
+//  Copyright © 2016 EmbeddedSources. All rights reserved.
 //
 
 import Foundation
